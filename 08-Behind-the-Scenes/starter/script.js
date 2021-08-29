3277091e44abd34d0908ae86c874ba563e929308
fediushkin <fediushkin@fim.uni-passau.de>
2021-08-30 00:28:23 +0200

    scopes examples uploaded

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    
+    function printAge() {
+        let output = `${firstName} ,you are ${age}, born in ${birthYear}`
+        console.log(output);
+
+        if (birthYear >=1981 && birthYear <=1996) {
+            const firstName = 'Steven';
+            const str = `Oh you are a millenial, ${firstName}`
+            console.log(str);
+
+            // reassigning variable 'output' in block scope
+            output = "NEW OUTPUT!"
+
+            function add(a,b) {
+                return a + b;
+            }
+        }
+        //console.log(add(2,2));
+        console.log(output);
+    }
+
+    printAge();
+
+    return age;
+};
+
+const firstName = 'Jonas';
+
+calcAge(1991);
+
